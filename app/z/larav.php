5e3a183f1c2236e2e1efb49e663429b216bf4c29
ben <ben@x24.fr>
2021-01-21 19:04:30 +0100

    laravel replace database query

--- app/z/larav.php
+++ app/z/larav.php
@@ -1,12 +1,13 @@
 <?php
 #gu;list;phpx ~/home/ehpad/app/z/larav.php
-namespace Alptech\Wip;chdir(__DIR__);require_once'../autoload.php';#todo:rename to ajax
-
-$ids=fun::fgc('/c/desk/etablissements-prescripteurs.txt');
+namespace Alptech\Wip;die;chdir(__DIR__);require_once'../autoload.php';#todo:rename to ajax
+$h='larav';
+#$h='laravProd';
+if(1){
+#$ids=io::fgc('/c/Users/ben/Desktop/etablissements-prescripteurs.txt');
+$ids=file_get_contents('c:/Users/ben/Desktop/etablissements-prescripteurs.txt');
 $a=1;
-
-
-
-$x=fun::sql("update health_organizations set prescripteur=1 where id in(".$ids.")",'larav');
-$x=fun::sql("select * from health_organizations limit 10",'larav');
+$x=fun::sql("update health_organizations set prescripteur=1 where id in(".$ids.")",$h);#laravProd
+}
+$x=fun::sql("select prescripteur,id from health_organizations where prescripteur=1 limit 10",$h);
 $a=1;
