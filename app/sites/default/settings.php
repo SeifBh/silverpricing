1a16f1c9a0117b9b95a8fc3fdf121c0a5bbb0acd
ben <ben@x24.fr>
2021-02-12 16:18:27 +0100

    correctifRecherche Dashboard residence

--- app/sites/default/settings.php
+++ app/sites/default/settings.php
@@ -2,9 +2,9 @@
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT & ~E_NOTICE & ~E_WARNING);#overrides drupal base exluding notices
 $databases = ['default' => ['default' => [
 #drushy en -y prixres3;
-'database' => 'silverpricing_db_dev',
+#'database' => 'silverpricing_db_dev',
 #'database' => 'silverpricing_db_prod',
-      #'database' => 'silverpricing_db',
+'database' => 'silverpricing_db',
       'username' => 'silverpricing',
       'password' => 'Silverpicing@wynter@2020',
       'host' => 'localhost',
