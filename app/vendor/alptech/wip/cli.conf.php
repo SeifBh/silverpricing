d9b93fcfda4485b5e65384471976be7779e410e7
ben <ben@x24.fr>
2021-03-31 19:20:49 +0200

    composer cleanup

--- app/vendor/alptech/wip/cli.conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Alptech\Wip;
-
-if(isset($_SERVER['WINDIR'])){
-    return ['cliHost' => 'https://ehpad.home/', 'cliDocRoot' => 'C:\Users\ben\home\ehpad\app/'];
-}#else prod
-return ['cliHost' => 'http://ehpad.silverpricing.fr/', 'cliDocRoot' => '/home/ubuntu/SilverPricing/public_html/app.silverpricing.fr/'];
